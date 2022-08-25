16969ee07be073fc5b3bd233dd3e4e66c57c52b4
Michael Niblett <mike@leecher.niblett.nl>
2022-08-25 19:56:49 +0200

    - cleaned up output - fixed upsert (instead of doing nothing) - not inserting rows that are empty

--- doit.py
+++ doit.py
@@ -28,7 +28,7 @@ cur = conn.cursor()
 
 dates = []
 date_to = datetime.datetime.now()
-for i in range(0, 660):
+for i in range(0, 1):
     dates.append(date_to - datetime.timedelta(days=i))
 
 r = Remeha(config['remeha']['username'], config['remeha']['password'], ssl_verify=False);
@@ -75,6 +75,20 @@ Fields
     'verbruik_tapwater_cpree': 'Verbruik tapwater CPR+EE'
 }
 
+Monitoring:
+1    Stand-by
+2    Passief tapwater
+4    Tapwaterbedrijf voordraaitijd
+8    Passief CV
+16   Centraal verwarmingsbedrijf voordraaitijd
+32   Passief koelbedrijf actief
+128  Regenereren
+256  Noodbedrijf tapwaterbedrijf actief
+1028 Tapwaterbedrijf actief (compressor draait)
+1040 Centraal verwarmingsactief (compressor draait)
+2048 Elektrisch element actief
+3088 Centraal verwarmingsactief (compressor + elektrisch element aan)
+
 """
 count = 0
 for date_to in dates:
@@ -85,23 +99,32 @@ for date_to in dates:
 
     ## prepare query
     cols = ','.join(db_fields)
+    update_columns = ','.join([f'{field}=EXCLUDED.{field}' for field in db_fields])
     placeholders = ','.join(['%s' for m in db_fields])
-    query = f'INSERT INTO remeha ({cols}) VALUES ({placeholders}) ON CONFLICT (sample) DO NOTHING'
+    query = f'INSERT INTO remeha ({cols}) VALUES ({placeholders}) ON CONFLICT (sample) DO UPDATE SET {update_columns}'
     for row in data:
         values = []
+        if not row['tvu'] and not row['verbruik_cv_compressor']:
+            print(f'skipping row because {row}')
+            continue
         for key, value in row.items():
             if key == 'time':
-                values.append(row[key])
+                seconds = int(row[key][-2:])
+                if seconds > 29:
+                    seconds_rounded = '30'
+                else:
+                    seconds_rounded = '00'
+                sample = f'{row[key][:-2]}{seconds_rounded}'
+                values.append(sample)
             else:
                 if len(row[key]) > 0:
                     values.append(int(row[key].split(',')[0]))
                 else:
                     values.append(None)
-
         cur.execute(query, values)
         count += 1
-    print(f'Inserted {len(data)} rows for {date_to.strftime("%Y-%m-%d")}')
+    print(f'Inserted data from {len(data)} rows for {date_to.strftime("%Y-%m-%d")}')
+    conn.commit()
     sleep(1)
-conn.commit()
 
 print(f'inserted {count}')
--- usage.sql
+++ usage.sql
@@ -1,9 +1,11 @@
 SELECT 
 	TO_CHAR(sample, 'YYYY-MM'),  
 	MAX(verbruik_passief_koelen) - MIN(verbruik_passief_koelen) AS koelen, 
-	MAX(verbruik_cv_compressor) - MIN(verbruik_cv_compressor) AS verwarmen, 
-	MAX(verbruik_tapwater_compressor) - MIN(verbruik_tapwater_compressor) AS warm_water 
+	MAX(verbruik_cv_compressor) - MIN(verbruik_cv_compressor) AS verwarmen_wp, 
+	MAX(verbruik_cv_cpr_en_ee) - MIN(verbruik_cv_cpr_en_ee) AS verwarmen_element,
+	MAX(verbruik_tapwater_compressor) - MIN(verbruik_tapwater_compressor) AS warm_water, 
+	MAX(verbruik_standby) - MIN(verbruik_standby) AS standby
 FROM remeha 
-GROUP BY TO_CHAR(sample, 'YYYY-MM') 
+GROUP BY ROLLUP(TO_CHAR(sample, 'YYYY-MM') )
 ORDER BY TO_CHAR(sample, 'YYYY-MM') desc;
 
